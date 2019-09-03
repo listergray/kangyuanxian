3a0f7ac1699dd948d49ca6ecb7bbbcef00ef7319
ListerGray <listergray@e163.com>
2019-09-03 22:58:29 +0800

    first

---
+++ src/main/java/com/example/kangyuanxian/KangyuanxianApplication.java
@@ -0,0 +1,13 @@
+package com.example.kangyuanxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangyuanxianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KangyuanxianApplication.class, args);
+    }
+
+}
